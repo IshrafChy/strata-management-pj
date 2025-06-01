6c8672509bd9d0f373f24066d9e461cc41d7fb76
IshrafChy <ishrafchy17@outlook.com>
2025-06-01 20:52:12 +1000

    aaaa

---
+++ api/message.php
@@ -0,0 +1,6 @@
+<?php
+header('Content-Type: text/html');
+
+echo "<h1>Hello from PHP!</h1>";
+echo "<p>This is a message from a PHP file running on Vercel.</p>";
+?>
---
+++ public/message.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Message</title>
+</head>
+<body>
+    <p>Hello from PHP! This is a static message.</p>
+</body>
+</html>
