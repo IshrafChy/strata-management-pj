0cc33bbab7ada715358dfbe135de6f43d2e9091b
IshrafChy <ishrafchy17@outlook.com>
2025-06-01 19:21:30 +1000

    Update info.php

--- php/info.php
+++ php/info.php
@@ -1,4 +1,5 @@
 <?php
+// Added a comment to trigger a rebuild
 echo "<h1>PHP is running on Vercel!</h1>";
 echo "<p>This is a static PHP page.</p>";
 echo "<p>PHP Version: " . phpversion() . "</p>";
