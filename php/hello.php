412ae45f7547c2e86e64ce292c0f0bac99fb263d
IshrafChy <ishrafchy17@outlook.com>
2025-06-01 20:06:50 +1000

    hopeeeeeee

---
+++ php/hello.php
@@ -0,0 +1,5 @@
+<?php
+echo "<h1>Hello from a brand new PHP file on Vercel!</h1>";
+echo "<p>This page is intended to demonstrate basic PHP execution.</p>";
+echo "<p>PHP Version: " . phpversion() . "</p>";
+?> 
--- public/static-info.php
+++
@@ -1,4 +0,0 @@
-<h1>Hello from a Static PHP File on Vercel!</h1>
-<p>This file is served statically from the public directory.</p>
-<p>It does not require server-side PHP execution to be displayed.</p>
-<?php // This is just here to keep the .php extension, the code won't execute ?> 
